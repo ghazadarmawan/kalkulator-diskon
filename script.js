a28231784cdbdb814dd092b3ae61188650ed2a45
ghazadarmawan <ghazadarmawan@gmail.com>
2023-12-17 11:45:24 +0700

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculate() {
+    const usage = parseFloat(document.getElementById('usage').value);
+  
+    if (isNaN(usage) || usage < 0) {
+      alert('Masukkan jumlah pemakaian kWh yang valid.');
+      return;
+    }
+  
+    const pricePerKwh = 1467; // Harga per kWh PLN (misalnya)
+    const totalCost = usage * pricePerKwh;
+  
+    displayResult(totalCost);
+  }
+  
+  function displayResult(totalCost) {
+    const resultElement = document.getElementById('result');
+    resultElement.innerText = `Biaya listrik yang harus dibayar: Rp ${totalCost.toFixed(2)}`;
+  }
+  
